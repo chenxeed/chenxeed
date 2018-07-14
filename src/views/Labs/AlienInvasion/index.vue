7dc9c1c122ae625c656d94be8bb9ff0d8487013a
Albert ChenX <chenxeed@gmail.com>
2018-07-14 16:34:54 +0800

    (imp) add more detail on alien-invasion

--- src/views/Labs/AlienInvasion/index.vue
+++ src/views/Labs/AlienInvasion/index.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="container">
-    <p>A simple game to destroy Alien!</p>
-    <p>Made with the help of library jQuery and Raphael.js</p>
-    <p>Check it out <a target="_blank" href="https://chenxeed.github.io/alien-invasion">here</a></p>
+    <p>
+      A simple game to destroy Alien!
+      Made with the help of library <a href="http://jquery.com/" target="_blank">jQuery</a> and <a href="http://dmitrybaranovskiy.github.io/raphael/" target="_blank">Raphael.js</a>
+    </p>
+    <p>
+      <figure class="image is-128x128">
+        <img src="~./screenshot.png">
+      </figure>
+    </p>
+    <p>Play the game <a target="_blank" href="https://chenxeed.github.io/alien-invasion">here</a></p>
+    <p>Check the source code in <a target="_blank" href="https://github.com/chenxeed/alien-invasion">here</a></p>
   </div>
 </template>
 <script lang="ts">
